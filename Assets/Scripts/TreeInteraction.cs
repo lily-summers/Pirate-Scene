3c74072360d590909f96a8ac203bc837565ddc1b
Lily Summers <Isaac2.Henry@live.uwe.ac.uk>
2024-05-01 21:11:22 +0100

    coconut fall script

---
+++ Assets/Scripts/TreeInteraction.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeInteraction : MonoBehaviour
+{
+    public GameObject player;
+    public Rigidbody coconut;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(player.transform.position, this.transform.position);
+        if (dist < 3)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                Debug.Log("touch fuzzy, get dizzy\n");
+                coconut.useGravity = true;
+            }
+        }
+    }
+}
