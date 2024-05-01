0cd41172bec2fbcd69726597df0693c75f4c7a3f
Lily Summers <Isaac2.Henry@live.uwe.ac.uk>
2024-05-01 22:31:17 +0100

    Coconut interaction finished

--- Assets/Scripts/CoconutSmasher.cs
+++ Assets/Scripts/CoconutSmasher.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CoconutSmasher : MonoBehaviour
 {
+    public Animator animator;
+    public Rigidbody rb;
+    public GameObject island;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,16 @@ public class CoconutSmasher : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject);
+        if (collision.gameObject == island)
+        {
+            animator.SetTrigger("crack");
+            rb.isKinematic = true;
+            this.transform.eulerAngles = Vector3.zero;
+        }
     }
 }
